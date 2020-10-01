2d560e685ba01b2fff59d6f9cc6438d44c5603f4
Gabriel Cards <43912656+GabrielC4rds@users.noreply.github.com>
2020-10-01 11:28:57 -0300

    another up

--- src/pages/Podcast.js
+++ src/pages/Podcast.js
@@ -231,8 +231,8 @@ function Podcast() {
 
   const disqusShortname = "indiecacao"
   const disqusConfig = {
-    url: 'https://master.d3s7w3k063szjv.amplifyapp.com/}',
-    identifier: 'podcast',
+    url: "https://master.d3s7w3k063szjv.amplifyapp.com/}",
+    identifier: "podcast",
     title: "Title of Your Article"
   }
   return (
