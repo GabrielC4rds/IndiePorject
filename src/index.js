e7eb8f19186ed3e945f272197be53fe09ac8ce2b
Gabriel Cards <43912656+GabrielC4rds@users.noreply.github.com>
2020-10-13 10:44:51 -0300

    upzasso

--- src/index.js
+++ src/index.js
@@ -5,26 +5,26 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { StoreProvider } from './Store';
 import { HashRouter } from 'react-router-dom'
-import { hydrate, render } from "react-dom";
+// import { hydrate, render } from "react-dom";
 
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<StoreProvider><App /></StoreProvider>, rootElement);
-} else {
-  render(<StoreProvider><App /></StoreProvider>, rootElement);
-}
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<StoreProvider><App /></StoreProvider>, rootElement);
+// } else {
+//   render(<StoreProvider><App /></StoreProvider>, rootElement);
+// }
 
-// ReactDOM.render(
-//   <React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
 
-//     <StoreProvider>
+    <StoreProvider>
 
-//       <App />
-//     </StoreProvider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+      <App />
+    </StoreProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
