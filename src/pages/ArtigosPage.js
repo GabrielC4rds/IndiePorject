45839e5c5dbbb1ed8035a1ccf6bb41d7ea008075
Gabriel Cards <43912656+GabrielC4rds@users.noreply.github.com>
2020-10-16 09:26:42 -0300

    update atigos page pagination

--- src/pages/ArtigosPage.js
+++ src/pages/ArtigosPage.js
@@ -16,6 +16,34 @@ import Icon from '@ant-design/icons';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import Slider from "react-slick";
+import Pagination from 'reactjs-hooks-pagination';
+
+const pageLimit = 5;
+const initialState = {
+  user: {},
+  loading: true,
+  error: ''
+}
+
+const Reducer = (state, action) => {
+  switch (action.type) {
+    case 'OnSuccess':
+      return {
+        loading: false,
+        user: action.payload,
+        error: ''
+      }
+    case 'OnFailure':
+      return {
+        loading: false,
+        user: {},
+        error: 'Something went wrong'
+      }
+
+    default:
+      return state
+  }
+}
 
 const All = styled.div`
   font-family: 'Montserrat', sans-serif !important;
@@ -58,8 +86,8 @@ const TopDiv = styled.div`
 `;
 
 const PostImage = styled.div`
-  width: 25vw;
-  height: 25vh;
+  width: 550px;
+  height: 300px;
   cursor: pointer;
   background-size: 100% auto;
   background-repeat: no-repeat;
@@ -110,47 +138,128 @@ const DataText = styled.label`
   }
 `;
 
+const PageButtons = styled.div`
+  width: 100%;
+  height: 5vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PageButton = styled.div`
+  width: 4vw;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
+  margin-right: 10px;
+  label{
+    color: #fff;
+    font-size: 15px;
+    cursor: pointer;
+  }
+`;
+
 function ArtigosPage() {
 
   const [all, setAll] = useState([]);
   const [User, setUser] = useContext(StoreContext);
+  // const [state, dispatch] = useReducer(Reducer, initialState);
+  const [offset, setOffset] = useState(0);
+  const [totalRecords, setTotalRecords] = useState(50);
+  const [currentPage, setCurrentPage] = useState(1);
+  var itemsPerPage = 15;
 
+  async function handleNext() {
+    if (offset < totalRecords) {
+      var PreviousBtnStyle = await document.getElementById('previousBtn');
+      if (offset > 0) {
+        PreviousBtnStyle.style.display = "flex"
+      } else {
+        PreviousBtnStyle.style.display = "none";
+
+      }
+      setCurrentPage(offset);
+      setOffset(offset + itemsPerPage);
+    }
+  }
+
+  async function handlePrev() {
+    if (offset > itemsPerPage) {
+      var PreviousBtnStyle = await document.getElementById('previousBtn');
+      setOffset(offset - itemsPerPage);
+      setCurrentPage(offset - 2 * itemsPerPage);
+      if (offset > 2 * itemsPerPage) {
+        PreviousBtnStyle.style.display = "flex"
+      } else {
+        PreviousBtnStyle.style.display = "none";
+
+      }
+    }
+  }
   useEffect(() => {
     async function FetchMyApi() {
-      let url = await window.location.href.toString().replace('https://master.d3s7w3k063szjv.amplifyapp.com/', '');
+      var PreviousBtnStyle = await document.getElementById('previousBtn');
+      var NextBtnStyle = await document.getElementById('nextBtn');
+      let url = await window.location.href.toString().replace('https://indiecacao.com.br/', '');
       let urlTitle = await url.replace("%20", " ");
       let items = await ConnectContent();
       let allContent = await items.filter(x => x.fields.type == "artigo" || x.fields.type == "analise");
       setAll(allContent.reverse());
-
-       window.onpopstate = function() {
+      setCurrentPage(0);
+      setTotalRecords(allContent.length)
+      { console.log(allContent.length) }
+      setOffset(itemsPerPage);
+      window.onpopstate = function () {
         //blah blah blah
-        urlTitle? window.location.href = `/${urlTitle}` : window.location.href = "/";
-       }
+        urlTitle ? window.location.href = `/${urlTitle}` : window.location.href = "/";
+      }
+      PreviousBtnStyle.style.display = "none";
+      NextBtnStyle.style.display = "none";
+      if (itemsPerPage < allContent.length) {
+        NextBtnStyle.style.display = "flex";
+      } else {
+        NextBtnStyle.style.display = "none";
+      }
+      //  if(offset > 2){
+      //    PreviousBtnStyle.style.display= "block"
+      //   }else{
+      //     PreviousBtnStyle.style.display= "none";
+
+      //  }
     }
     FetchMyApi();
   }, []);
 
   return (
     <All>
+
       <Header />
       <TopDiv />
       <TitleDiv>Últimos Artigos</TitleDiv>
-      {all.map((res) => {
+      {all.slice(currentPage, offset).map((res) => {
         return (
 
-          <Item onClick={() => window.location.href=`/${res.fields.url}`}>
-            
-              <PostImage  onClick={() => setUser(res.fields.title)} style={{ backgroundImage: `url(${res.fields.bannerImage.fields.file.url})` }}></PostImage>
-              <TextDiv>
+          <Item onClick={() => window.location.href = `/${res.fields.url}`}>
 
-                <Title onClick={() => setUser(res.fields.title)}>{res.fields.artigoTitle ? res.fields.artigoTitle : res.fields.postTitle}</Title>
-                <DataText onClick={() => setUser(res.fields.title)}>{res.fields.dataSign}</DataText>
-              </TextDiv>
-            
+            <PostImage onClick={() => setUser(res.fields.title)} style={{ backgroundImage: `url(${res.fields.bannerImage.fields.file.url})` }}></PostImage>
+            <TextDiv>
+
+              <Title onClick={() => setUser(res.fields.title)}>{res.fields.artigoTitle ? res.fields.artigoTitle : res.fields.postTitle}</Title>
+              <DataText onClick={() => setUser(res.fields.title)}>{res.fields.dataSign}</DataText>
+            </TextDiv>
+
           </Item>
         )
       })}
+      <PageButtons>
+
+        <PageButton id="previousBtn" onClick={() => handlePrev()}> 
+        <label>PREVIOUS</label></PageButton>
+        <PageButton id="nextBtn" onClick={() => handleNext()}>
+          <label>NEXT</label>
+        </PageButton>
+      </PageButtons>
       <div style={{ width: "100%", height: "10vh" }} />
       <Footer />
     </All>
