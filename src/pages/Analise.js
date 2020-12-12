4e35f87491141c578d9c85d1ed4f19b04a33a61f
Gabriel Cards <43912656+GabrielC4rds@users.noreply.github.com>
2020-12-11 21:54:17 -0300

    Author tag insert

--- src/pages/Analise.js
+++ src/pages/Analise.js
@@ -172,7 +172,142 @@ const DisqusDiv = styled.div`
   margin-top: 5vh;
 `;
 
+const BottomDivider = styled.div`
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  div{
+    width: 45%;
+    height: 1.5px;
+    @media only screen and (max-width: 768px) {
+      width: 40%;
+    }
+  }
+  img{
+    width: 50px;
+    margin-right: 4px;
+  }
+`;
 
+const AuthorDiv = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const PhotoDiv = styled.div`
+  width: 180px;
+  height: 180px;
+  background: pink;
+  border-radius: 50%;
+  border: 5px solid #6D2AA6;
+  background-size: 110%;
+  background-position: center;
+  @media only screen and (max-width: 768px) {
+    width: 130px;
+    height: 130px;
+    margin: 20px 0;
+  }
+`;
+
+const AuthorTxt = styled.div`
+  width: 85%;
+  height: 100%;
+  margin-left: 30px;
+  line-height: 0 !important;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    margin: 0;
+    text-align: center;
+    width: 95%;
+  }
+`;
+
+const AuthorTop = styled.div`
+  width: 100%;
+  height: 5vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    height: 15vh;
+    
+  }
+`;
+
+const CircleDiv = styled.div`
+  width: 60px !important;
+  height: 60px !important;
+  background: linear-gradient(210deg, #56EE8D 0%, #6D2AA6 100%);
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 768px) {
+    width: 45px !important;
+    height: 45px !important;
+    img{
+      width: 10vw;
+    }
+  }
+  
+`;
+
+const RevTab = styled.div`
+  width: auto;
+  padding: 0 1vw;
+  height: 100%;
+  background: #6D2AA6;
+  border-radius: 10px;
+  color: #fff;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  font-size: 15px;
+  span{
+    font-weight: 600;
+    margin-left: 5px;
+  }
+  font-style: italic;
+  @media only screen and (max-width: 768px) {
+    width: auto;
+    padding: 0 3vw;
+    height: 50px;
+    margin-bottom: 10px;
+  }
+`;
+
+const AuthorTitle = styled.label`
+  font-size: 25px;
+  color: #6D2AA6;
+  font-weight: 700 !important;
+  margin: 0 !important;
+  @media only screen and (max-width: 768px) {
+    margin: 10px 0 !important;
+  }
+
+`;
+
+const AuthorDesc = styled.label`
+  font-size: 17px !important;
+  margin: 0 !important;
+  color: #000;
+  margin-left: 10px;
+  line-height: 1.5 !important;
+  @media only screen and (max-width: 768px) {
+    text-align: center !important;
+    
+  }
+`;
 function Analise() {
 
   const [User, setUser] = useContext(StoreContext)
@@ -323,6 +458,26 @@ function Analise() {
                   )
                 })}
               </ul>
+              <BottomDivider>
+                <div style={{background: "#6D2AA6"}}/>
+                 <CircleDiv>
+
+                  <img src="./icon/miniLogo.png"/>
+                 </CircleDiv>
+                <div style={{background: "#56EE8D"}}/>
+              </BottomDivider>
+              <AuthorDiv>
+                <PhotoDiv style={{backgroundImage: ` url(${res.fields.author.fields.image.fields.file.url})`}}></PhotoDiv> 
+                <AuthorTxt>
+                  <AuthorTop>
+
+                    <AuthorTitle>{res.fields.author.fields.name}</AuthorTitle>
+                    <RevTab>{res.fields.reviewer}</RevTab>
+                  </AuthorTop>
+                  <AuthorDesc>{res.fields.author.fields.desc}</AuthorDesc>
+                </AuthorTxt>
+              </AuthorDiv> 
+              <Divider/>
               <DisqusDiv>
                 <Disqus.DiscussionEmbed
                   shortname={disqusShortname}
--- src/pages/Artigo.js
+++ src/pages/Artigo.js
@@ -164,7 +164,141 @@ const SpaceImage = styled.div`
 const DisqusDiv = styled.div`
   margin-top: 5vh;
 `;
+const BottomDivider = styled.div`
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  div{
+    width: 45%;
+    height: 1.5px;
+    @media only screen and (max-width: 768px) {
+      width: 40%;
+    }
+  }
+  img{
+    width: 50px;
+    margin-right: 4px;
+  }
+`;
 
+const AuthorDiv = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const PhotoDiv = styled.div`
+  width: 180px;
+  height: 180px;
+  background: pink;
+  border-radius: 50%;
+  border: 5px solid #6D2AA6;
+  background-size: 110%;
+  background-position: center;
+  @media only screen and (max-width: 768px) {
+    width: 130px;
+    height: 130px;
+    margin: 20px 0;
+  }
+`;
+
+const AuthorTxt = styled.div`
+  width: 85%;
+  height: 100%;
+  margin-left: 30px;
+  line-height: 0 !important;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+const AuthorTop = styled.div`
+  width: 100%;
+  height: 5vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    height: 15vh;
+    
+  }
+`;
+
+const CircleDiv = styled.div`
+  width: 60px !important;
+  height: 60px !important;
+  background: linear-gradient(210deg, #56EE8D 0%, #6D2AA6 100%);
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 768px) {
+    width: 45px !important;
+    height: 45px !important;
+    img{
+      width: 10vw;
+    }
+  }
+  
+`;
+
+const RevTab = styled.div`
+  width: auto;
+  padding: 0 1vw;
+  height: 100%;
+  background: #6D2AA6;
+  border-radius: 10px;
+  color: #fff;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  font-size: 15px;
+  span{
+    font-weight: 600;
+    margin-left: 5px;
+  }
+  font-style: italic;
+  @media only screen and (max-width: 768px) {
+    width: auto;
+    padding: 0 3vw;
+    height: 50px;
+    margin-bottom: 10px;
+  }
+`;
+
+const AuthorTitle = styled.label`
+  font-size: 25px;
+  color: #6D2AA6;
+  font-weight: 700 !important;
+  margin: 0 !important;
+  @media only screen and (max-width: 768px) {
+    margin: 10px 0 !important;
+  }
+
+`;
+
+const AuthorDesc = styled.label`
+  font-size: 17px !important;
+  margin: 0 !important;
+  color: #000;
+  margin-left: 10px;
+  line-height: 1.5 !important;
+  @media only screen and (max-width: 768px) {
+    text-align: center !important;
+    
+  }
+`;
 const linkSpotify = 'https://open.spotify.com/embed-podcast/episode/58haVRLxRGvDOMoT7reNGB';
 
 function Artigo() {
@@ -186,7 +320,7 @@ function Artigo() {
       // setAll(allContent)
       // :
       // setAll(allContent);
-      // console.log("all", all);
+      console.log("all", allContent);
       setDisquisId(name);
       setDisquisUrl(nameUrl);
       let url = await window.location.href.toString().replace('https://indiecacao.com.br/', '');
@@ -298,7 +432,7 @@ function Artigo() {
               {res.fields.conclusaoImage1?<><ContentImage style={{backgroundImage: `url(${res.fields.conclusaoImage1.fields.file.url})`}}/>
               <SpaceImage/></>
               :null}
-              {/* <BottomDivider>
+               <BottomDivider>
                 <div style={{background: "#6D2AA6"}}/>
                  <CircleDiv>
 
@@ -307,18 +441,17 @@ function Artigo() {
                 <div style={{background: "#56EE8D"}}/>
               </BottomDivider>
               <AuthorDiv>
-                <PhotoDiv style={{backgroundImage: " url('./icon/moebus.png')"}}></PhotoDiv>
+                <PhotoDiv style={{backgroundImage: ` url(${res.fields.author.fields.image.fields.file.url})`}}></PhotoDiv> 
                 <AuthorTxt>
                   <AuthorTop>
 
-                    <AuthorTitle>Matheus "MobiusNero" Moebus</AuthorTitle>
-                    <RevTab>Revisão: Natasha Harumi</RevTab>
+                    <AuthorTitle>{res.fields.author.fields.name}</AuthorTitle>
+                    <RevTab>Revisão: <span> {res.fields.reviewer}</span></RevTab>
                   </AuthorTop>
-                  <AuthorDesc>Estudante de Física e viciado em videogames desde pequeno. Entrou no Indiecação para mostrar 
-                    aos outros sua paixão (e fazê-los se apaixonarem) pelos nossos queridos indies.</AuthorDesc>
+                  <AuthorDesc>{res.fields.author.fields.desc}</AuthorDesc>
                 </AuthorTxt>
-              </AuthorDiv> */}
-              {/* <Divider/> */}
+              </AuthorDiv> 
+              <Divider/>
               <DisqusDiv>
                 <Disqus.DiscussionEmbed
                   shortname={disqusShortname}
