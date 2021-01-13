2e20730d86ed614d30101181e3f26fe3365cf795
Gabriel Cards <43912656+GabrielC4rds@users.noreply.github.com>
2021-01-13 15:21:35 -0300

    minor fixes global an theme

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+export const lightTheme = {
+  body: '#fff',
+  text: '#000',
+  toggleBorder: '#FFF',
+  gradient: 'linear-gradient(#39598A, #79D7ED)',
+}
+
+export const darkTheme = {
+  body: '#000',
+  text: '#FAFAFA',
+  toggleBorder: '#6B8096',
+  gradient: 'linear-gradient(#091236, #1E215D)',
+}
+
+
+
+
+
