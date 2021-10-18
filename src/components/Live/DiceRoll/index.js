49ed113428092caceaf1d4f5ead440d5ac20e5d6
Gabriel Cards <43912656+GabrielC4rds@users.noreply.github.com>
2021-10-17 22:51:15 -0300

    componente de dados criado e funcional
    todo o funcionamento da rolagem de dados com bonus adicionado
    
    Falta: terminar de passar as props e ajustar o layout com hayato

--- src/components/Live/DiceRoll/index.js
+++ src/components/Live/DiceRoll/index.js
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
-import React from 'react';
+import React, { useState} from 'react';
 
 const Content = styled.div`
-  display: flex;
-  width: 407px;
-  height: 379px;
-  justify-content: space-around;
-  flex-direction: column;
   align-items: center;
   background: #FFFFFF 0% 0% no-repeat padding-box;
-  box-shadow: 0px 3px 10px #00000029;
   border-radius: 16px;
+  box-shadow: 0px 3px 10px #00000029;
+  display: flex;
+  flex-direction: column;
+  height: 379px;
+  justify-content: space-around;
   margin: 70px;
   position: relative;
+  width: 407px;
   z-index: 0;
   p{
     font: normal normal 600 16px/22px Noto Sans;
@@ -22,11 +22,12 @@ const Content = styled.div`
 `;
 
 const Infs = styled.div`
-  width: 90%;
-  display: flex;
-  margin: 20px 0;
-  justify-content: space-between;
   align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 5px;
+  margin-top: 20px;
+  width: 90%;
   p{
     color: #1C2126;
     margin: 0;
@@ -39,11 +40,11 @@ const Infs = styled.div`
 `;
 
 const Roll20 = styled.div`
-  width: 90%;
-  height: 20vh;
   display: flex;
   flex-direction: column;
+  height: 15vh;
   justify-content: space-between;
+  width: 90%;
   div{
     display: flex;
     justify-content: space-between;
@@ -56,31 +57,37 @@ const Roll20 = styled.div`
 `;
 
 const ButtonDiv = styled.div`
-  width: 108px;
-  height: 48px;
   background: #F2F4F8 0% 0% no-repeat padding-box;
   border-radius: 16px;
   cursor: pointer;
+  display: flex;
+  height: 48px;
+  align-items: center !important;
+  justify-content: center !important;
+  width: 108px;
+  img{
+    width: 21px;
+  }
 `;
 
 const InputDiv = styled.input`
-  width: 108px;
-  height: 48px;
   background: #F2F4F8 0% 0% no-repeat padding-box;
   border-radius: 16px;
   border: none;
+  height: 48px;
   text-align: center;
+  width: 108px;
 `;
 
 const RollButton = styled.div`
-  width: 371px;
-  height: 48px;
-  display: flex;
-  justify-content: center !important;
   align-items: center !important;
   background: #4525F2 0% 0% no-repeat padding-box;
-  cursor: pointer;
   border-radius: 100px;
+  cursor: pointer;
+  display: flex;
+  height: 48px;
+  justify-content: center !important;
+  width: 371px;
   p{
     font: normal normal 600 16px/22px Noto Sans;
     color: #FFFFFF;
@@ -94,7 +101,68 @@ const SpaceLine = styled.div`
   width: 100%;
 `;
 
+const Result = styled.div`
+  display: flex;
+  height: 23%;
+  justify-content: space-between;
+  width: 90%;
+  img{
+    width: 20px;
+    height: auto;
+    margin-top: 15px;
+  }
+`;
+
+const BoxResult = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 85%;
+  width: 27%;
+  label{
+    color: #99A1A9;
+    font-size: 14px;
+    font-weight: 600;
+    margin-bottom: 7px;
+  }
+  div{
+    background: #F2F4F8 0% 0% no-repeat padding-box;
+    border-radius: 16px;
+    border: none;
+    display: flex;
+    height: 48px;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    p{
+      margin: 0;
+    }
+  }
+`;
+
+
 function DiceRoll() {
+  var Signal = "plus";
+  var signData
+  const [numbData, setNumbData] = useState(0)
+  const [d20, setd20] = useState(0);
+  const [bonus, setBonus] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const changeDataSign = () => {
+    let number = document.getElementById("number");
+    let bonus= parseFloat(number.value);
+    let diceNumber = Math.floor(Math.random()*20+1);
+    signData = Math.sign(number.value)
+    signData >= 0 ?
+      Signal = "plus" :
+      Signal = "minus"
+      console.log(Signal)
+      document.getElementById('sigImg').src = "../icon/" + Signal + ".svg"
+      setd20(diceNumber)
+      setBonus(Math.abs(bonus))
+      setResult(diceNumber + bonus)
+  }
 
   return (
     <Content>
@@ -106,15 +174,33 @@ function DiceRoll() {
       <Roll20>
         <p>Bônus</p>
         <div>
-          <ButtonDiv>
+          <ButtonDiv onClick={() => setNumbData(numbData - 1)}>
+            <img src="../icon/minusBox.svg"></img>
           </ButtonDiv>
-          <InputDiv placeholder="0"></InputDiv>
-          <ButtonDiv></ButtonDiv>
+          <InputDiv value={numbData} id="data" placeholder="0" id="number"></InputDiv>
+          <ButtonDiv onClick={() => setNumbData(numbData + 1)}>
+            <img src="../icon/plusBox.svg" ></img>
+          </ButtonDiv>
         </div>
-        <RollButton><p>Rolar d20</p></RollButton>
+        <RollButton onClick={() => changeDataSign()}><p>Rolar d20</p></RollButton>
         <SpaceLine />
       </Roll20>
-      {/* <Result></Result> */}
+      <Result>
+        <BoxResult>
+          <label>D20</label>
+          <div><p>{d20}</p></div>
+        </BoxResult>
+        <img id="sigImg" src="../icon/plus.svg"/>
+        <BoxResult>
+          <label>Bônus</label>
+          <div><p>{bonus}</p></div>
+        </BoxResult>
+        <img src="../icon/equal.svg" />
+        <BoxResult>
+          <label>Resultado</label>
+          <div><p>{result}</p></div>
+        </BoxResult>
+      </Result>
 
     </Content>
   );
